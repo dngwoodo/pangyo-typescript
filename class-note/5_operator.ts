a271905e279d605992118ddf29c8bd738fbfc426
dngwoo <akfmzk84@naver.com>
2020-11-04 21:18:08 +0900

    Union Type의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,7 +1,15 @@
 // Union Type - 하나이상의 타입을 사용할 수 있게 해준다.
 function logMessage(value: string | number){
-    console.log(value)
+    if(typeof value === 'number'){
+        value.toLocaleString(); // any를 쓰면 자동완성이 되지 않음. 해당 타입에 따라 api를 사용하려면 유니온타입을 써야함.
+    }
+    if(typeof value === 'string'){
+        value.toString();
+    }
+    throw new TypeError('value must be string or number'); // 에러 발생
 }
 
 logMessage('hello');
 logMessage(100);
+
+// 타입 가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
