ec006679df9375b12089aba31cf483befc397a95
dngwoo <akfmzk84@naver.com>
2020-11-04 21:11:04 +0900

    type-aliases vs interface

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -20,3 +20,7 @@ var str: MyString = 'hello';
 
 type Todo = { id: string; title: string; done: boolean }
 function getTodo(todo: Todo){}
+
+// type-aliases vs interface
+// 둘의 가장 큰 차이점은 타입의 확장 가능 / 불가능이다.
+// 인터페이스는 확장이 가능하지만 타입별칭은 불가능하다.
