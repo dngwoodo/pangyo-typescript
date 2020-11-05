075515f50f8e30e0148ff64737d7d551aae36c05
dngwoo <akfmzk84@naver.com>
2020-11-05 20:18:11 +0900

    제네릭을 사용하지 않았을 경우 하드코딩 된 예제

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -7,9 +7,31 @@
 // logText('hi');
 // logText(true);
 
-function logText<T>(text: T): T{
+// function logText<T>(text: T): T{
+//     console.log(text);
+//     return text;
+// }
+
+// logText<string>('hi') // 넘길 타입을 여기서 정한다.
+
+
+// 제네릭을 사용하지 않으면 이렇게 중복코드를 적게 된다.
+function logText(text: string){
     console.log(text);
-    return text;
+    text.split('').reverse().join('');
+    return Text;
 }
 
-logText<string>('hi') // 넘길 타입을 여기서 정한다.
+function logNumber(num: number){
+    console.log(num);
+    return num;
+}
+
+logText('a');
+logText(10);
+const num = logNumber(10);
+logText(true);
+
+logText('a');
+logText(10);
+logText(true);
