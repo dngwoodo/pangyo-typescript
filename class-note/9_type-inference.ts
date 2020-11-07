b56520fc53edc6f19201f7b2c33d81c4914367c5
dngwoo <akfmzk84@naver.com>
2020-11-07 13:25:13 +0900

    제네릭에서의 타입추론

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 var a = 10;
 
 function getA(b = 10) { // getA에 마우스 호버를 해보면 타입추론된 값이 나온다.
@@ -8,3 +9,31 @@ function getA(b = 10) { // getA에 마우스 호버를 해보면 타입추론된
 10 + '190' // 1010 <- 문자와 숫자를 더하면 문자로 나옴
 
 
+// 타입 추론 기본 2
+interface Dropdown<T> {
+    value: T;
+    title: string;
+}
+
+var shoppingItem: Dropdown<string> = {
+    value: 'abc',
+    title: 'hello'
+}
+
+// 타입 추론 기본 3
+interface Dropdown<T>{
+    value: T;
+    title: string;
+}
+
+interface DropdownDetail<K> extends Dropdown<K>{ // DropdownDetail에 제네릭 값을 받아서 Dropdown 제네릭에도 전달
+    description: K;
+}
+
+var obj22: DropdownDetail<string> = {
+    // value, title, description 셋중 하나만 없어도 에러 뜸.
+    // 타입도 추론해서 설정해줌.
+    value: 'aa',
+    title: '111',
+    description: '222222'
+}
