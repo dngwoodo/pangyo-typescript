1ea50c5c2159b4a9bd145c2e428b7128b8a940d3
dngwoo <akfmzk84@naver.com>
2020-11-07 15:18:52 +0900

    fix: 타입 가드 예제 수정 / 타입 가드 사용법

--- class-note/11_type-guard.ts
+++ class-note/11_type-guard.ts
@@ -1,24 +1,38 @@
-interface Developer {
+interface Developer5 {
     name: string;
     skill: string;
 }
 
-interface Person {
+interface Person5 {
     name: string;
     age: number;
 }
 
-function introduce(): Developer | Person {
+function introduce(): Developer5 | Person5 {
     return { name: 'Tony', age: 33, skill: 'Iron Making'}
 }
 
-var tony = introduce();
-console.log(tony.name); // skill, age는 불가함.
+var tony2 = introduce();
+console.log(tony2.name); // skill, age는 불가함.
 
-if((tony as Developer).skill){ // type assertion(타입 단언)을 통해 사용 가능.
-    var skill = (tony as Developer).skill;
+if((tony2 as Developer5).skill){ // type assertion(타입 단언)을 통해 사용 가능.
+    var skill = (tony2 as Developer5).skill;
     console.log(skill);
-} else if((tony as Person).age){ // type assertion(타입 단언)을 통해 사용 가능.
-    var age = (tony as Developer).age;
+} else if((tony2 as Person5).age){ // type assertion(타입 단언)을 통해 사용 가능.
+    var age = (tony2 as Person5).age;
     console.log(age);
+}
+
+
+// 타입 가드 정의
+function isDeveloper(target: Developer5 | Person5): target is Developer5 { // 타입 가드 함수
+    return (target as Developer5).skill !== undefined; // skill이 존재한다면 Developer타입이다.
+}
+
+if(isDeveloper(tony2)){
+    tony2.skill // 에러 안남
+    tony2.age // 에러
+} else {
+    tony2.age // 에러 안남
+    tony2.skill // 에러
 }
