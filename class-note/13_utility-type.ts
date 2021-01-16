03eca30d0df6f543d386c3fd9c537d86090f591a
dngwoo <akfmzk84@naver.com>
2021-01-16 16:35:34 +0900

    유틸리티 타입 사례 - Pick

---
+++ class-note/13_utility-type.ts
@@ -0,0 +1,26 @@
+export {}
+// 백엔드에서 받아오는 데이터
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    stock: number;
+}
+
+// 상품 목록을 받아오기 위한 API 함수
+function fetchProducts(): Promise<Product[]> { // return 값이 없으므로 잠시 오류 남. 신경 안써도 된다.
+    // ..
+}
+
+// Pick이라는 유틸리티 타입을 이용하여 ProductDetail이라는 인터페이스를 따로 만들지 않고 사용가능하다.
+// interface ProductDetail {
+//     id: number;
+//     name: string;
+//     price: number;
+// }
+type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
+
+function displayProductDetail(shoppingItem: ShoppingItem){
+
+}
