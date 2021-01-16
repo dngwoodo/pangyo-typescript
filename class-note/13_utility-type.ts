363802d2a394bc188eeea94fb9a92e6a99cb3c6c
dngwoo <akfmzk84@naver.com>
2021-01-16 16:45:44 +0900

    유틸리티 타입 사례 - Partial

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -29,3 +29,19 @@ function displayProductDetail(shoppingItem: ShoppingItem){
 // 2. Omit
 // Pick이라는 반대이다. 적어준 프로퍼티를 제외한 것들을 사용한다.
 type ShoppingItem2 = Omit<Product, 'brand' | 'stock'>
+
+
+// 3. Partial
+// 백엔드에 있는 상품정보를 업데이트하게 되면 어떤 정보를 업데이트할지 매번 바뀌게 된다.
+// 그럴 때 사용하는 것이 optional properties인데 이 부분을 Partial을 이용하여 사용해보자.
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+type UpdateProduct = Partial<Product>
+
+function updateProductItem(productItem: UpdateProduct){}
