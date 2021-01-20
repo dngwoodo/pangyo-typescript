9e40e7d7ef389eef1cc37219156eca7316f301c8
dngwoodo <dngwoodo@gmail.com>
2021-01-21 07:13:07 +0900

    [실습 풀이] 특정 국가의 사망자 정보 목록 설정 함수 타입 정의

--- project/src/app.ts
+++ project/src/app.ts
@@ -7,6 +7,7 @@ import {
   Country,
   CountrySummaryResponse,
   CovidSummaryResponse,
+  CountrySummaryInfo,
 } from './covid/index';
 
 // utils
@@ -122,15 +123,16 @@ async function handleListClick(event: MouseEvent) {
   isDeathLoading = false;
 }
 
-function setDeathsList(data: any) {
+function setDeathsList(data: CountrySummaryResponse) {
   const sorted = data.sort(
-    (a: any, b: any) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
+    (a: CountrySummaryInfo, b: CountrySummaryInfo) =>
+      getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
   );
-  sorted.forEach((value: any) => {
+  sorted.forEach((value: CountrySummaryInfo) => {
     const li = document.createElement('li');
     li.setAttribute('class', 'list-item-b flex align-center');
     const span = document.createElement('span');
-    span.textContent = value.Cases;
+    span.textContent = value.Cases.toString();
     span.setAttribute('class', 'deaths');
     const p = document.createElement('p');
     p.textContent = new Date(value.Date).toLocaleDateString().slice(0, -1);
